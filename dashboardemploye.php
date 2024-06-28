13a1147333d146a0256e04d1905b4c75a243dd04
soufgame <nhailasoufiane2003@gmail.com>
2024-06-28 16:06:53 +0100

    enleve le light mode et le darkmode

--- dashboardemploye.php
+++ dashboardemploye.php
@@ -191,10 +191,7 @@ $stmtTicketsResolus->close();
           <button id="menu_bar">
             <span class="material-symbols-sharp">menu</span>
           </button>
-          <div class="theme-toggler">
-            <span class="material-symbols-sharp active">light_mode</span>
-            <span class="material-symbols-sharp">dark_mode</span>
-          </div>
+          
           <div class="profile">
             <div class="info">
               <p><b><?php echo htmlspecialchars($username); ?></b></p>
